c4fc6edfc821f567df0e3f9683974bb7a94614c3
KFUABWNFIAOWKFZT23481 <vincentmyeealt@gmail.com>
2024-07-27 11:05:28 -0400

    Update chatbypassv3.lua

--- chatbypassv3.lua
+++ chatbypassv3.lua
@@ -3,74 +3,163 @@
 
 -- Instances:
 
-
-
-local ScreenGui = Instance.new("ScreenGui")
+local BB = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
-local CanvasFrame = Instance.new("CanvasGroup")
-local UIListLayout = Instance.new("UIListLayout")
 local TextBox = Instance.new("TextBox")
 local UIPadding = Instance.new("UIPadding")
+local Shadows = Instance.new("Folder")
+local AmbientShadow = Instance.new("ImageLabel")
+local PenumbraShadow = Instance.new("ImageLabel")
+local UmbraShadow = Instance.new("ImageLabel")
 local TextButton = Instance.new("TextButton")
-local UIPadding_2 = Instance.new("UIPadding")
+local TextButton_2 = Instance.new("TextButton")
+local Shadows_2 = Instance.new("Folder")
+local AmbientShadow_2 = Instance.new("ImageLabel")
+local PenumbraShadow_2 = Instance.new("ImageLabel")
+local UmbraShadow_2 = Instance.new("ImageLabel")
 
 --Properties:
 
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ResetOnSpawn = false
+BB.Name = "BB"
+BB.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-Frame.Parent = ScreenGui
+Frame.Parent = BB
 Frame.AnchorPoint = Vector2.new(0.5, 0.5)
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundColor3 = Color3.fromRGB(42, 42, 42)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.499235511, 0, 0.5, 0)
-Frame.Size = UDim2.new(0, 366, 0, 260)
-
-CanvasFrame.Parent = Frame
-CanvasFrame.BackgroundTransparency = 1
-CanvasFrame.Name = "Frame"
-CanvasFrame.Position = UDim2.new(0, 0,0.096, 0)
-CanvasFrame.Size = UDim2.new(1, 0,-0.096, 208)
-
-UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Left
-UIListLayout.VerticalAlignment=Enum.VerticalAlignment.Bottom
-UIListLayout.FillDirection=Enum.FillDirection.Vertical
-UIListLayout.Parent=CanvasFrame
+Frame.Position = UDim2.new(0.5, 0, 0.871774197, 0)
+Frame.Size = UDim2.new(0, 454, 0, 33)
 
 TextBox.Parent = Frame
-TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.BackgroundTransparency = 1.000
+TextBox.BackgroundColor3 = Color3.fromRGB(26, 26, 26)
 TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextBox.BorderSizePixel = 0
-TextBox.Position = UDim2.new(0, 0, 0.80384618, 0)
-TextBox.Size = UDim2.new(1, 0, 0, 51)
-TextBox.Font = Enum.Font.Code
-TextBox.PlaceholderText = "Chat bypass here..."
+TextBox.Position = UDim2.new(0.075000003, 0, 0.129999995, 0)
+TextBox.Size = UDim2.new(0, 417, 0, 25)
+TextBox.Font = Enum.Font.GothamMedium
+TextBox.PlaceholderText = "made by creysound"
 TextBox.Text = ""
 TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.TextSize = 16.000
+TextBox.TextSize = 14.000
 TextBox.TextXAlignment = Enum.TextXAlignment.Left
 
 UIPadding.Parent = TextBox
-UIPadding.PaddingLeft = UDim.new(0, 18)
+UIPadding.PaddingLeft = UDim.new(0, 5)
+
+Shadows.Name = "Shadows"
+Shadows.Parent = Frame
+
+AmbientShadow.Name = "AmbientShadow"
+AmbientShadow.Parent = Shadows
+AmbientShadow.AnchorPoint = Vector2.new(0.5, 0.5)
+AmbientShadow.BackgroundTransparency = 1.000
+AmbientShadow.BorderColor3 = Color3.fromRGB(27, 42, 53)
+AmbientShadow.Position = UDim2.new(0.5, 0, 0.5, 3)
+AmbientShadow.Size = UDim2.new(1, 5, 1, 5)
+AmbientShadow.ZIndex = 0
+AmbientShadow.Image = "rbxassetid://1316045217"
+AmbientShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+AmbientShadow.ImageTransparency = 0.800
+AmbientShadow.ScaleType = Enum.ScaleType.Slice
+AmbientShadow.SliceCenter = Rect.new(10, 10, 118, 118)
+
+PenumbraShadow.Name = "PenumbraShadow"
+PenumbraShadow.Parent = Shadows
+PenumbraShadow.AnchorPoint = Vector2.new(0.5, 0.5)
+PenumbraShadow.BackgroundTransparency = 1.000
+PenumbraShadow.BorderColor3 = Color3.fromRGB(27, 42, 53)
+PenumbraShadow.Position = UDim2.new(0.5, 0, 0.5, 1)
+PenumbraShadow.Size = UDim2.new(1, 18, 1, 18)
+PenumbraShadow.ZIndex = 0
+PenumbraShadow.Image = "rbxassetid://1316045217"
+PenumbraShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+PenumbraShadow.ImageTransparency = 0.880
+PenumbraShadow.ScaleType = Enum.ScaleType.Slice
+PenumbraShadow.SliceCenter = Rect.new(10, 10, 118, 118)
+
+UmbraShadow.Name = "UmbraShadow"
+UmbraShadow.Parent = Shadows
+UmbraShadow.AnchorPoint = Vector2.new(0.5, 0.5)
+UmbraShadow.BackgroundTransparency = 1.000
+UmbraShadow.BorderColor3 = Color3.fromRGB(27, 42, 53)
+UmbraShadow.Position = UDim2.new(0.5, 0, 0.5, 6)
+UmbraShadow.Size = UDim2.new(1, 10, 1, 10)
+UmbraShadow.ZIndex = 0
+UmbraShadow.Image = "rbxassetid://1316045217"
+UmbraShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+UmbraShadow.ImageTransparency = 0.860
+UmbraShadow.ScaleType = Enum.ScaleType.Slice
+UmbraShadow.SliceCenter = Rect.new(10, 10, 118, 118)
 
 TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BackgroundTransparency = 1.000
 TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.92900002, 0, 0, 0)
-TextButton.Size = UDim2.new(0.0710000023, 0, -0.0649999976, 50)
-TextButton.Font = Enum.Font.Code
-TextButton.Text = "X"
-TextButton.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextButton.Size = UDim2.new(0, 34, 0, 33)
+TextButton.Font = Enum.Font.MontserratBold
+TextButton.Text = "BB"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.TextSize = 14.000
-TextButton.TextXAlignment = Enum.TextXAlignment.Right
 
-UIPadding_2.Parent = TextButton
-UIPadding_2.PaddingRight = UDim.new(0, 10)
+TextButton_2.Parent = BB
+TextButton_2.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.791284382, 0, 0.601612926, 0)
+TextButton_2.Size = UDim2.new(0, 100, 0, 100)
+TextButton_2.Visible = false
+TextButton_2.Font = Enum.Font.MontserratBold
+TextButton_2.Text = "BB"
+TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.TextSize = 18.000
 
+Shadows_2.Name = "Shadows"
+Shadows_2.Parent = TextButton_2
+
+AmbientShadow_2.Name = "AmbientShadow"
+AmbientShadow_2.Parent = Shadows_2
+AmbientShadow_2.AnchorPoint = Vector2.new(0.5, 0.5)
+AmbientShadow_2.BackgroundTransparency = 1.000
+AmbientShadow_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
+AmbientShadow_2.Position = UDim2.new(0.5, 0, 0.5, 3)
+AmbientShadow_2.Size = UDim2.new(1, 5, 1, 5)
+AmbientShadow_2.ZIndex = 0
+AmbientShadow_2.Image = "rbxassetid://1316045217"
+AmbientShadow_2.ImageColor3 = Color3.fromRGB(0, 0, 0)
+AmbientShadow_2.ImageTransparency = 0.800
+AmbientShadow_2.ScaleType = Enum.ScaleType.Slice
+AmbientShadow_2.SliceCenter = Rect.new(10, 10, 118, 118)
+
+PenumbraShadow_2.Name = "PenumbraShadow"
+PenumbraShadow_2.Parent = Shadows_2
+PenumbraShadow_2.AnchorPoint = Vector2.new(0.5, 0.5)
+PenumbraShadow_2.BackgroundTransparency = 1.000
+PenumbraShadow_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
+PenumbraShadow_2.Position = UDim2.new(0.5, 0, 0.5, 1)
+PenumbraShadow_2.Size = UDim2.new(1, 18, 1, 18)
+PenumbraShadow_2.ZIndex = 0
+PenumbraShadow_2.Image = "rbxassetid://1316045217"
+PenumbraShadow_2.ImageColor3 = Color3.fromRGB(0, 0, 0)
+PenumbraShadow_2.ImageTransparency = 0.880
+PenumbraShadow_2.ScaleType = Enum.ScaleType.Slice
+PenumbraShadow_2.SliceCenter = Rect.new(10, 10, 118, 118)
+
+UmbraShadow_2.Name = "UmbraShadow"
+UmbraShadow_2.Parent = Shadows_2
+UmbraShadow_2.AnchorPoint = Vector2.new(0.5, 0.5)
+UmbraShadow_2.BackgroundTransparency = 1.000
+UmbraShadow_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
+UmbraShadow_2.Position = UDim2.new(0.5, 0, 0.5, 6)
+UmbraShadow_2.Size = UDim2.new(1, 10, 1, 10)
+UmbraShadow_2.ZIndex = 0
+UmbraShadow_2.Image = "rbxassetid://1316045217"
+UmbraShadow_2.ImageColor3 = Color3.fromRGB(0, 0, 0)
+UmbraShadow_2.ImageTransparency = 0.860
+UmbraShadow_2.ScaleType = Enum.ScaleType.Slice
+UmbraShadow_2.SliceCenter = Rect.new(10, 10, 118, 118)
 
 
 -- Table mappings for bypassing the chat filter
@@ -185,15 +274,16 @@ local chat = function(_string)
 end
 
 local baitfire = function()
-  game.Players:Chat(bait[math.random(1, #bait)])
+  --game.Players:Chat(bait[math.random(1, #bait)])
 end
+
 
 -- Scripts:
 
-local function AYPMA_fake_script() -- TextBox.LocalScript 
+local function IMBAH_fake_script() -- TextBox.LocalScript 
 	local script = Instance.new('LocalScript', TextBox)
 
-		function KeyD(key)
+	function KeyD(key)
 			key = key:lower()
 			if key == "e" then
 				wait()
@@ -201,105 +291,89 @@ local function AYPMA_fake_script() -- TextBox.LocalScript
 			end
 		end
 		game.Players.LocalPlayer:GetMouse().KeyDown:connect(KeyD)
-	
+		
 	script.Parent.FocusLost:connect(function(enterPressed)
-		if enterPressed and script.Parent.Text ~= "" then 
+			if enterPressed and script.Parent.Text ~= "" then 
 baitfire()
-
-local msg = replace(TextBox.Text)
-local returnedmessage = ""
-local text = msg .. ""
-local b = "͟"
-if game.TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
-	chat(msg)
-else
-	for i = 1, (200 - string.len(msg)) do
-	local letter = b
-	text = text ..  letter
-	returnedmessage = text
-	end
-	chat(returnedmessage)
-end
-
-baitfire()
-
-			local TextLabel = Instance.new("TextLabel")
-	local UIPadding = Instance.new("UIPadding")
 	
-	--Properties:
-	
-	TextLabel.Parent = CanvasFrame
-	TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel.BackgroundTransparency = 1.000
-	TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	TextLabel.BorderSizePixel = 0
-	TextLabel.Position = UDim2.new(0, 0, 0, 0)
-	TextLabel.Size = UDim2.new(1, 0, -0.11057692, 40)
-	TextLabel.Font = Enum.Font.Code
-	TextLabel.Text = "["..game.Players.LocalPlayer.DisplayName.."]: "..script.Parent.Text
-	TextLabel.TextColor3 = Color3.fromRGB(0, 255, 0)
-	TextLabel.TextSize = 16.000
-	TextLabel.TextXAlignment = Enum.TextXAlignment.Left
-	
-	UIPadding.Parent = TextLabel
-	UIPadding.PaddingLeft = UDim.new(0, 18)
+	local msg = replace(TextBox.Text)
+	local returnedmessage = ""
+	local text = msg .. ""
+	local b = "͟"
+	if game.TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
+		chat(msg)
+	else
+		for i = 1, (200 - string.len(msg)) do
+		local letter = b
+		text = text ..  letter
+		returnedmessage = text
 		end
-	end)
+		chat(returnedmessage)
+	end
+	
+	baitfire()
 end
-coroutine.wrap(AYPMA_fake_script)()
-local function ZLXYK_fake_script() -- Frame.Dragify 
+end)
+end
+coroutine.wrap(IMBAH_fake_script)()
+local function VOYIY_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
-	local UIS = game:GetService("UserInputService")
-	function dragify(Frame)
-	    dragToggle = nil
-	    local dragSpeed = 0
-	    dragInput = nil
-	    dragStart = nil
-	    local dragPos = nil
-	    function updateInput(input)
-	        local Delta = input.Position - dragStart
-	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
-	    end
-	    Frame.InputBegan:Connect(function(input)
-	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
-	            dragToggle = true
-	            dragStart = input.Position
-	            startPos = Frame.Position
-	            input.Changed:Connect(function()
-	                if input.UserInputState == Enum.UserInputState.End then
-	                    dragToggle = false
-	                end
-	            end)
-	        end
-	    end)
-	    Frame.InputChanged:Connect(function(input)
-	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-	            dragInput = input
-	        end
-	    end)
-	    game:GetService("UserInputService").InputChanged:Connect(function(input)
-	        if input == dragInput and dragToggle then
-	            updateInput(input)
-	        end
-	    end)
-	end
 	
-	dragify(script.Parent)
+		local UIS = game:GetService("UserInputService")
+		function dragify(Frame)
+		    dragToggle = nil
+		    local dragSpeed = 0
+		    dragInput = nil
+		    dragStart = nil
+		    local dragPos = nil
+		    function updateInput(input)
+		        local Delta = input.Position - dragStart
+		        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+		        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
+		    end
+		    Frame.InputBegan:Connect(function(input)
+		        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+		            dragToggle = true
+		            dragStart = input.Position
+		            startPos = Frame.Position
+		            input.Changed:Connect(function()
+		                if input.UserInputState == Enum.UserInputState.End then
+		                    dragToggle = false
+		                end
+		            end)
+		        end
+		    end)
+		    Frame.InputChanged:Connect(function(input)
+		        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		            dragInput = input
+		        end
+		    end)
+		    game:GetService("UserInputService").InputChanged:Connect(function(input)
+		        if input == dragInput and dragToggle then
+		            updateInput(input)
+		        end
+		    end)
+		end
+		
+		dragify(script.Parent)
 end
-coroutine.wrap(ZLXYK_fake_script)()
-local function FTNAN_fake_script() -- TextButton.LocalScript 
+coroutine.wrap(VOYIY_fake_script)()
+local function PHACYTE_fake_script() -- TextButton.LocalScript 
 	local script = Instance.new('LocalScript', TextButton)
 
 	script.Parent.MouseButton1Down:Connect(function()
-		script.Parent.Parent.Parent:Destroy()
+		script.Parent.Parent.Parent.Frame.Visible = false
+		script.Parent.Parent.Parent.TextButton.Visible = true
 	end)
 end
-coroutine.wrap(FTNAN_fake_script)()
+coroutine.wrap(PHACYTE_fake_script)()
+local function CSFZY_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
 
-coroutine.wrap(function()
-while wait(3) do
-	baitfire()
-		end
-	end)()
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Frame.Visible = true
+		script.Parent.Parent.TextButton.Visible = false
+	end)
+end
+coroutine.wrap(CSFZY_fake_script)()
